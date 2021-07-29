637408f8f60e0df7184f17a99a21fa9c44b6bec8
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-07-29 20:20:30 +0600

    Directly export combinators module

--- src/Valida.hs
+++ src/Valida.hs
@@ -1,45 +1,17 @@
 module Valida
     ( Selector
+    -- | Re-exports of "Valida.Combinators".
+    , module Valida.Combinators
     , Validation (..)
     , ValidationRule
     , Validator
-    , andAlso
-    , atleastContains
-    , atleastContains'
-    , buildValidator
-    , failureIf
-    , failureIf'
-    , failureUnless
-    , failureUnless'
     , fromEither
-    , label
-    , maxLengthOf
-    , maxLengthOf'
-    , maxValueOf
-    , maxValueOf'
-    , minLengthOf
-    , minLengthOf'
-    , minValueOf
-    , minValueOf'
-    , mustBe
-    , mustBe'
-    , mustContain
-    , mustContain'
-    , onlyContains
-    , onlyContains'
-    , orElse
-    , satisfyAll
-    , satisfyAny
     , toEither
     , vrule
     , (-?-)
-    , (<?>)
     ) where
 
-import Valida.Combinators     (andAlso, atleastContains, atleastContains', failureIf, failureIf', failureUnless,
-                               failureUnless', label, maxLengthOf, maxLengthOf', maxValueOf, maxValueOf', minLengthOf,
-                               minLengthOf', minValueOf, minValueOf', mustBe, mustBe', mustContain, mustContain',
-                               onlyContains, onlyContains', orElse, satisfyAll, satisfyAny, (<?>))
+import Valida.Combinators
 import Valida.Validation      (Validation (..))
 import Valida.ValidationRule  (ValidationRule (..), vrule)
 import Valida.ValidationUtils (fromEither, toEither)

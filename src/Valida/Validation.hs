87977b2bdb7c1ea8ce74c7634ef6dddfb27dfb19
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-08-04 18:45:42 +0600

    Remove redundant constraint from `Validation` Semigroup instance

--- src/Valida/Validation.hs
+++ src/Valida/Validation.hs
@@ -31,7 +31,7 @@ instance Semigroup e => Applicative (Validation e) where
 {- |
 * '(<>)' behaves similar to the 'Either' semigroup. i.e Returns the first 'Success'. But also accumulates 'Failure's.
 -}
-instance (Semigroup e, Semigroup a) => Semigroup (Validation e a) where
+instance Semigroup e => Semigroup (Validation e a) where
     s@(Success _) <> _             = s
     _             <> s@(Success _) = s
     Failure x     <> Failure y     = Failure $ x <> y

92de267abcef4498f4c6e7589146a5d34e4277c9
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-08-11 15:40:39 +0600

    Add unified non empty list type

--- test/Gen.hs
+++ test/Gen.hs
@@ -3,13 +3,19 @@
 
 module Gen
     ( ValidationQ (..)
+    , NonEmptyLQ
     ) where
 
-import Test.SmallCheck.Series (Serial (..), cons1, (\/))
-import Test.Tasty.QuickCheck  (Arbitrary (arbitrary, shrink), oneof)
+import qualified Data.List.NonEmpty as NE
+
+import           Test.SmallCheck.Series (Serial (..), cons1, (\/))
+import           Test.Tasty.QuickCheck  (Arbitrary (arbitrary, shrink), oneof)
+import qualified Test.Tasty.QuickCheck  as QC
+
 
 import Valida (Validation (..))
 
+-- | Wrapper around 'Validation' with 'Arbitrary' and 'Serial' instances.
 newtype ValidationQ e a = ValidationQ (Validation e a)
   deriving (Show)
 
@@ -20,3 +26,23 @@ instance (Arbitrary e, Arbitrary a) => Arbitrary (ValidationQ e a) where
 
 instance (Serial m e, Serial m a) => Serial m (ValidationQ e a) where
     series = ValidationQ <$> cons1 Failure \/ cons1 Success
+
+-- | Type unifying the separate `NonEmpty` impls on quickcheck and smallcheck.
+data NonEmptyLQ a = NonEmptyListQ (QC.NonEmptyList a) | NonEmptyQ (NE.NonEmpty a)
+  deriving (Show)
+
+instance Arbitrary a => Arbitrary (NonEmptyLQ a) where
+    arbitrary = NonEmptyListQ <$> arbitrary
+    shrink (NonEmptyListQ l) = NonEmptyListQ <$> shrink l
+    shrink _                 = error "Should not try to shrink NonEmptyQ"
+
+instance Serial m a => Serial m (NonEmptyLQ a) where
+    series = cons1 NonEmptyQ
+
+instance Functor NonEmptyLQ where
+    fmap f (NonEmptyListQ l) = NonEmptyListQ $ f <$> l
+    fmap f (NonEmptyQ l) = NonEmptyQ $ f <$> l
+
+instance Foldable NonEmptyLQ where
+    foldMap f (NonEmptyListQ l) = foldMap f $ QC.getNonEmpty l
+    foldMap f (NonEmptyQ l) = foldMap f l

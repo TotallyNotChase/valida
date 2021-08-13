73cfc7f3c1eb218d7a01920225932307858ae32c
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-08-13 13:23:27 +0400

    Improve documentation for `Validator`

--- src/Valida/Validator.hs
+++ src/Valida/Validator.hs
@@ -10,48 +10,28 @@ import Valida.Validation (Validation (..))
 -- | Convenience alias for functions that "select" a record field.
 type Selector a b = a -> b
 
-{- | An applicative validator. Validates a predicate on an input when run.
-
-The Functor instance maps a function over the 'Validation' result by re-using 'fmap' on it.
-
-The Applicative instance can be understood as-
-
-@
-(Validator ff) <*> (Validator v) = Validator
-    $ (\\inp -> ff inp <*> v inp)
-@
-
-i.e Run __ff__ and __v__ on the input, and compose the results (of type 'Validation') with '(<*>)'.
-
-The 'pure' implementation creates a 'Validator' that, when run, takes __any__ input, /ignores it/, and returns
-the given value wrapped in 'Success'.
--}
+-- | An applicative validator. Validates a predicate on an input when run and returns the 'Validation' result.
 newtype Validator e inp a = Validator { validate :: inp -> Validation e a }
 
+{- |
+* 'fmap' maps given function over the 'Validation' result by re-using 'fmap' on it.
+-}
 instance Functor (Validator e inp) where
-    -- | Run the validator function, and __fmap__ given function over the result.
     fmap f (Validator v) = Validator $ fmap f . v
 
+{- |
+* 'pure' creates a 'Validator' that always yields given value wrapped in 'Success', ignoring its input.
+* '(<*>)' runs 2 validators to obtain the 2 'Validation' results and combines them with '(<*>)'.
+This can be understood as-
+
+    @
+    (Validator ff) \<*\> (Validator v) = Validator (\\inp -> ff inp \<*\> v inp)
+    @
+
+    i.e Run __ff__ and __v__ on the input, and compose the 'Validation' results with '(<*>)'.
+-}
 instance Semigroup e => Applicative (Validator e inp) where
-    -- | Validator taking any input and returning given value in __Success__.
     pure = Validator . const . Success
-    {- | With minimal pointfree, this is equivalent to-
-
-    @
-    (Validator ff) <*> (Validator v) = Validator
-        $ (\\inp -> ff inp <*> v inp)
-    @
-
-    i.e Run __ff__ and __v__ on the input, and compose the results (of type __Validation__) with __<*>__.
-    With __ApplicativeDo__, this can be understood as-
-
-    @
-    (Validator ff) <*> (Validator v) = Validator (\\inp -> do
-        f <- ff inp
-        a <- v inp
-        pure (f a))
-    @
-    -}
     Validator ff <*> Validator v = Validator $ liftA2 (<*>) ff v
 
 {- |
